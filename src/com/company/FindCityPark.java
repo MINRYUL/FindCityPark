215f0244c21345bad4b309552cff2f9472174d16
HJH <goharrm@ajou.ac.kr>
2020-12-06 23:35:19 +0900

    correct all errors

--- src/com/company/FindCityPark.java
+++ src/com/company/FindCityPark.java
@@ -1,8 +1,8 @@
 package com.company;
 
-import java.sql.*;
-import java.util.ArrayList;
-import java.util.Scanner;
+        import java.sql.*;
+        import java.util.ArrayList;
+        import java.util.Scanner;
 
 public class FindCityPark {
 
@@ -10,10 +10,10 @@ public class FindCityPark {
         Connection con = null;
         Statement st = null;
         ResultSet rs = null;
-        //알아서 바꾸셈
+
         String url = "jdbc:postgresql://localhost/";
         String user = "postgres";
-        String password = "sanghee0610";
+        String password = "사용자비밀번호";
 
         GetSeoulParkData.getParkData();
         GetAirCondition.getAirCondition();
@@ -27,12 +27,14 @@ public class FindCityPark {
             Class.forName("org.postgresql.Driver");
             con = DriverManager.getConnection(url, user, password);
             st = con.createStatement();
-            st.executeUpdate("drop Table Park");
-            st.executeUpdate("drop Table Department");
-            st.executeUpdate("drop Table Review");
-            st.executeUpdate("drop Table AirCondition");
 
-            // System.out.println("Creating Park, Department, Review relations");
+            System.out.println("Connecting succeed");
+
+            st.executeUpdate("drop Table Park cascade");
+            st.executeUpdate("drop Table Department cascade");
+            st.executeUpdate("drop Table Review cascade");
+            st.executeUpdate("drop Table AirCondition cascade");
+            
             // 3개 테이블 생성: Create table문 이용
             st.executeUpdate("create table Park(parkId int not null, parkName text not null, parkDescription text not null, parkAddress text, region varchar(100) not null, parkTelephone varchar(50), parkUrl text, dpName varchar(100) not null, PRIMARY KEY(parkId));");
             st.executeUpdate("create table Department(dpName varchar(100) not null,dpAddress text, dpTelephone varchar(50), PRIMARY KEY(dpName));");
@@ -41,16 +43,8 @@ public class FindCityPark {
 
             // 디비에 삽입
             ArrayList<Park> parkList = GetSeoulParkData.getParkArrayList();
-            System.out.println(parkList.get(12).getParkId());
-            System.out.println(parkList.get(12).getParkName());
-            System.out.println(parkList.get(12).getParkDescription());
-            System.out.println(parkList.get(12).getParkAddress());
-            System.out.println(parkList.get(12).getRegion());
-            System.out.println(parkList.get(12).getParkTelephon());
-            System.out.println(parkList.get(12).getParkUrl());
-            System.out.println(parkList.get(12).getDpName());
-
             ArrayList<AirCondition> airConditionArrayList = GetAirCondition.getAirConditionArrayList();
+
             for(int i = 0; i < 25; i++){
                 if (airConditionArrayList.get(i).getRegion() == "111123") continue;
                 st.executeUpdate("insert into AirCondition values (" + "'" + airConditionArrayList.get(i).getRegion() + "', " +
@@ -59,13 +53,12 @@ public class FindCityPark {
                         "'" + airConditionArrayList.get(i).getUltraFineDust() + "');");
             }
 
-//            ArrayList<Park> parkList = GetSeoulParkData.getParkArrayList();
             for(int i = 0; i < parkList.size(); i++){
-                String text = parkList.get(i).getParkDescription().replace("'", "*");
+                String descText = parkList.get(i).getParkDescription().replace("'", "*");
                 st.executeUpdate("insert into Park values (" + "'" + parkList.get(i).getParkId() + "', " + "'" + parkList.get(i).getParkName() + "', " +
-                        "'" + text + "', " + "'" + parkList.get(i).getParkAddress() + "', " +
+                        "'" + descText + "', " + "'" + parkList.get(i).getParkAddress() + "', " +
                         "'" + parkList.get(i).getRegion() + "', " + "'" + parkList.get(i).getParkTelephon() + "', " +
-                        "'" + parkList.get(i).getParkUrl() + "', " + "'" + parkList.get(i).getDpName() + "');");
+                        "'" + parkList.get(i).getParkUrl() + "', '" + parkList.get(i).getDpName() + "');");
             }
 
             //Review 삽입
@@ -84,19 +77,22 @@ public class FindCityPark {
             st.executeUpdate("insert into Department values('서대문구청 푸른도시과', '서울 서대문구 연희로 248', '02-330-1301');");
             st.executeUpdate("insert into Department values('성동구청 공원녹지과', '서울 성동구 고산자로 270', '02-2286-5114');");
 
-            String userRegion; // 거주지역(구) 입력받기
-            String parkId;     // parkId 입력받기
-            String dpName;     // 리뷰볼 때 join하기 위해 사용됨
+            String userRegion = null; // 거주지역(구) 입력받기
+            String parkId = null;     // parkId 입력받기
+            String dpName = null;     // 리뷰볼 때 join하기 위해 사용됨
 
-            System.out.println("지역(구)을 입력하세요 : ");
+            System.out.print("지역구를 입력하세요 : ");
             userRegion = scan.nextLine();
 
-            rs = st.executeQuery("select parkId, dpName\n" +
-                    "from Park \n" +
-                    "where region = " + userRegion + ";");
+            rs = st.executeQuery("select parkId, dpName " +
+                    "from Park " +
+                    "where region = '" + userRegion + "';");
 
-            parkId = rs.getString("parkId");
-            dpName = rs.getString("dpName");
+            while(rs.next()) {
+                parkId = rs.getString("parkId");
+                dpName = rs.getString("dpName");
+            }
+
             // 위 쿼리를 통해서 parkName 들을 보여주기
 
             // parkName 하나 입력받기
@@ -106,44 +102,38 @@ public class FindCityPark {
             // 1. 공원 소개
             rs = st.executeQuery("select parkDescription \n" +
                     "from Park \n" +
-                    "where parkId = " + parkId + " and region = " + userRegion + ";");
+                    "where parkId = " + parkId + " and region = '" + userRegion + "';");
 
             // parkDescription(공원소개) 출력하기
 
             // 2. 위치보기
             rs = st.executeQuery("select parkAddress \n" +
                     "from Park \n" +
-                    "where parkId = " + parkId + " and region = " + userRegion + ";");
+                    "where parkId = " + parkId + " and region = '" + userRegion + "';");
 
             // parkAddress출력
 
-            //3. 시설 소개
-            rs = st.executeQuery("select parkEquip\n" +
-                    "from Park \n" +
-                    "where parkId = " + parkId + " and region = " + userRegion + ";");
-
-            // parkEquip 출력
-
-            // 4. 현재 대기 환경
+            // 3. 현재 대기 환경
             rs = st.executeQuery("select airStatus, ozone, carbon, fineDust, ultraFineDust \n" +
                     "from Park, AirCondition \n" +
-                    "where Park.region = AirCondition.region and " + " region = " + userRegion + ";");
+                    "where Park.region = AirCondition.region and " + " Park.region = '" + userRegion + "';");
 
             // 대기환경 다 출력
 
-            // 5. 리뷰 보기
+            // 4. 리뷰 보기
             rs = st.executeQuery("select parkName, writer, star, reviewContent \n" +
                     "from Park, Review\n" +
-                    "where Park.parkId = Review.parkId and Park.parkId = " + parkId + " and region = " + userRegion +";");
+                    "where Park.parkId = Review.parkId and Park.parkId = " + parkId + " and region = '" + userRegion +"';");
 
             // 리뷰 다 출력하기
 
-            // 6. 관리부서 보기
-            rs = st.executeQuery("select dpName, dpAddress, dpTelephone\n" +
+            // 5. 관리부서 보기
+            rs = st.executeQuery("select parkName, Park.dpName, dpAddress, dpTelephone\n" +
                     "from Park, Department\n" +
-                    "where Park.dpName and Department.dpName and Park.parkId = " + parkId + "Park.dpName = " + dpName + ";");
+                    "where Park.dpName = Department.dpName and Park.parkId = " + parkId + " and Park.dpName = '" + dpName + "';");
 
             // 관리부서 출력
+            
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
